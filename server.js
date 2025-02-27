cc357dc3f88f2a40935c4fc88e05829cf570e48b
am499702@gmail.com <am499702@gmail.com>
2025-02-27 18:52:51 +0530

    Port change

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Database Connected"))
   .catch((err) => console.error(err));
-
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
